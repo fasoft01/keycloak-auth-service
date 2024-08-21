687c5937f704460c6f0834551f6223ef5e0f521c
Fasoft <101709684+FARAMATSI@users.noreply.gitlab.com>
2024-08-21 12:17:19 +0200

    initial commit

---
+++ src/test/java/zw/co/fasoft/keycloakauthservice/KeycloakAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.fasoft.keycloakauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
